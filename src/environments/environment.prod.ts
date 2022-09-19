c15aa8317526a6c29fab121380739f5998fb80a4
unknown <saikiran.m@varuns2017outlook.onmicrosoft.com>
2022-09-20 00:53:16 +0530

    prod builf

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   baseUrl: 'https://api.hisense-india.com',
-  websiteUrl: 'https://api.hisense-india.com'
+  websiteUrl: 'https://www.hisense-india.com'
 };
